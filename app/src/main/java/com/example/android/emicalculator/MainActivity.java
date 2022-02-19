c4c1ddb70b35eaee9195ec8025e773a5cedfacc4
aswayampakula <aswayampakula@islander.tamucc.edu>
2022-02-18 21:58:31 -0600

    Initial commit

--- app/src/main/java/com/example/android/emicalculator/MainActivity.java
+++ app/src/main/java/com/example/android/emicalculator/MainActivity.java
@@ -6,83 +6,79 @@ import android.view.View;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Button;
+import android.widget.Toast;
 
-import com.google.android.gms.ads.AdView;
-
-
-import java.lang.Math;
 
 public class MainActivity extends AppCompatActivity {
-    double emi = 0;
-    private AdView mAdView;
-
+    EditText loanAmt,annualInterest;//Declaring loan amount and AnnualInterest
+    TextView years5,years10,years15,years20,years25,years30;// declaring years from 5 to 30
+    Button calculate;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-    }
+        loanAmt= (EditText) findViewById(R.id.loanAmt);
+        annualInterest= (EditText) findViewById(R.id.annualInterest);
+        years5= (TextView) findViewById(R.id.years5);
+        years10= (TextView) findViewById(R.id.years10);
+        years15= (TextView) findViewById(R.id.years15);
+        years20= (TextView) findViewById(R.id.years20);
+        years25= (TextView) findViewById(R.id.years25);
+        years30= (TextView) findViewById(R.id.years30);
 
-    /* This method is called when the calculate button is called
-    * Double.parseDouble used for converting String to Double variable
-    * Integer.parseInt used for converting String to int variable
+        calculate= (Button) findViewById(R.id.calculate);//Initializing the button
+        calculate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try{
 
-     */
-    public void calculate (View view) {
+                    String amountText=loanAmt.getText().toString();//getting text from amount and interest
+                    String interestText=annualInterest.getText().toString();
 
-        EditText LoanAmount = (EditText) findViewById(R.id.loan_amount);
-        if( LoanAmount.getText().toString().length() == 0 )
-        {LoanAmount.setError( "Loan Amount is required" );}
-        else {
-            double principal = Double.parseDouble(LoanAmount.getText().toString());
+                    double amount=Double.parseDouble(amountText);//converting the string to double
+                    double interest=Double.parseDouble(interestText);
+                    double monthlyInterest=getMonthlyInterest(interest);
+                    /**
+                     * Calculating the monthly payment for years 5,10,15,20,25 and 30
+                     */
+                    double monthlyPayment5=getMonthlyPayment(amount,5,monthlyInterest);
+                    double monthlyPayment10=getMonthlyPayment(amount,10,monthlyInterest);
+                    double monthlyPayment15=getMonthlyPayment(amount,15,monthlyInterest);
+                    double monthlyPayment20=getMonthlyPayment(amount,20,monthlyInterest);
+                    double monthlyPayment25=getMonthlyPayment(amount,25,monthlyInterest);
+                    double monthlyPayment30=getMonthlyPayment(amount,30,monthlyInterest);
 
-            EditText LoanInterest = (EditText) findViewById(R.id.loan_interest);
-            if (LoanInterest.getText().toString().length() == 0)
-            {LoanInterest.setError("Loan Interest is required!");}
-            else {
-                double interest = Double.parseDouble(LoanInterest.getText().toString());
-
-                EditText LoanTenure = (EditText) findViewById(R.id.loan_tenure);
-                if (LoanTenure.getText().toString().length() == 0)
-                {LoanTenure.setError("Loan Tenure is required!");}
-                else {
-                    int tenure = Integer.parseInt(LoanTenure.getText().toString());
-
-                    double Emi = calculateEmiSummery(principal, interest, tenure, emi);
-                    displayMessage(Emi);
+                    years5.setText("$"+String.format("%.2f",monthlyPayment5));//updating the text fields
+                    years10.setText("$"+String.format("%.2f",monthlyPayment10));
+                    years15.setText("$"+String.format("%.2f",monthlyPayment15));
+                    years20.setText("$"+String.format("%.2f",monthlyPayment20));
+                    years25.setText("$"+String.format("%.2f",monthlyPayment25));
+                    years30.setText("$"+String.format("%.2f",monthlyPayment30));
+                }catch (Exception e){
+                    Toast.makeText(getApplicationContext(),"Invalid input",Toast.LENGTH_SHORT).show();
                 }
             }
-        }
-
-
-    }
-
-    /*This Method is for calulation of EMI
-
-     */
-
-    private double calculateEmiSummery( double LoanAmount, double LoanInterest, int LoanTenure, double emi){
-        double messege = emi;
-        double intPerMonth = (LoanInterest/12/100);
-        messege = (LoanAmount*intPerMonth*((Math.pow((1+intPerMonth),LoanTenure))/(((Math.pow((1+intPerMonth),LoanTenure)))-1)));
-        return messege;
-
+        });
 
     }
+    //method to find monthly payment
+    static double getMonthlyPayment(double loanAmt, int numYears,
+                                    double monthlyInterest) {
 
-    /*
-    this Method display the Emi in the emi TextView
-    *Math.round is used for round offing the variable
-    *Double.toString is used for converting the double variable to string
-     */
-    private void displayMessage(Double message) {
-        double roundEmi = Math.round( message * 100.0 ) / 100.0;
-        String stringEmi= Double.toString(roundEmi);
-        TextView emiTextView = (TextView) findViewById(R.id.emi);
-        emiTextView.setText("₹ " + stringEmi);
+        int numMonths=numYears*12;
+        double monthlyPaymentRate = loanAmt * monthlyInterest
+                * (Math.pow(1 + monthlyInterest, numMonths))
+                / (Math.pow(1 + monthlyInterest, numMonths) - 1);
+        return monthlyPaymentRate;
     }
 
-
-
-
+    /**
+     a method to calculate monthly interest
+     **/
+    static double getMonthlyInterest(double interestRate){
+        double monthlyInterest = ((interestRate / 100) / 12);
+        return monthlyInterest;
+    }
 }
